5332cd9efa8bffbf7f703f24974f3c0d642239eb
Deddy Dayag <daviddayag@gmail.com>
2022-12-20 11:47:33 +0200

    minor help fix to deployment.sh deployment pv 1.0.11 twap to staging

--- deployer.sh
+++ deployer.sh
@@ -8,7 +8,7 @@ This tool will pull, tag, push and update the deployment file.
 Use with caution!
 
 usage: ./deployer.sh [deployment-file] [docker-org] [docker-repo] [build-docker-Image] [output-version]
-i.e. ./deployer.sh orbsnetworkstaging vm-twap v1.0.0-a14ff39d v1.0.10
+i.e. ./deployer.sh staging.json orbsnetworkstaging vm-twap v1.0.0-a14ff39d v1.0.10
 
 HELP
 
@@ -20,12 +20,12 @@ dockerRepo="$3"
 buildDockerImage="$4"
 version="$5"
 
-#echo "pulling buildDockerImage ${buildDockerImage} from ${dockerRepo}"
-#docker pull ${dockerOrg}/${dockerRepo}:${buildDockerImage}
-#echo "tagging buildDockerImage ${buildDockerImage} ==> ${version}-immediate"
-#docker tag ${dockerOrg}/${dockerRepo}:${buildDockerImage} ${dockerOrg}/${dockerRepo}:${version}-immediate
-#echo "pushing buildDockerImage ${dockerOrg}/${dockerRepo}:${version}-immediate"
-#docker push ${dockerOrg}/${dockerRepo}:${version}-immediate
+echo "pulling buildDockerImage ${buildDockerImage} from ${dockerRepo}"
+docker pull ${dockerOrg}/${dockerRepo}:${buildDockerImage}
+echo "tagging buildDockerImage ${buildDockerImage} ==> ${version}-immediate"
+docker tag ${dockerOrg}/${dockerRepo}:${buildDockerImage} ${dockerOrg}/${dockerRepo}:${version}-immediate
+echo "pushing buildDockerImage ${dockerOrg}/${dockerRepo}:${version}-immediate"
+docker push ${dockerOrg}/${dockerRepo}:${version}-immediate
 
 echo "modifying ${deploymentFile} to include the new image version"
 sed "s/${dockerOrg}\/${dockerRepo}:.*immediate/${dockerOrg}\/${dockerRepo}:${version}-immediate/g" ${deploymentFile} > ${deploymentFile}_temp && mv ${deploymentFile}_temp ${deploymentFile}
